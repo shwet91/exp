b72d2703ef1712f34b9f9b08897d41f8cbb8aa68
unknown <shwetparkash91@gmail.com>
2025-01-20 23:22:11 +0530

    1

--- frontend/auth/signup/script.js
+++ frontend/auth/signup/script.js
@@ -1,29 +1,42 @@
+const profilePic = document.getElementById('profile-picture');
+const CoverPic = document.getElementById('cover-image');
+const fullname = document.getElementById('fullname');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const coverImage = document.getElementById('cover-image');
+
+
+const newData = {
+    username: `${username.value}`,
+    fullname: `${fullname.value}`,
+    email: `${email.value}`,
+    password: `${password.value}`,
+    avatar: `${profilePic.value}`,
+    coverImage: `${coverImage.value}`
+}
+
+
+
+
+
 document.getElementById('myForm').addEventListener('submit', function(event) {
-    event.preventDefault();  // Prevent the default form submission
+    event.preventDefault();  // Prevent default form submission
 
-    // Create a FormData object from the form
-    const formData = new FormData(this);
 
-    console.log(typeof(formData));
+    const formData = new FormData(this);  // Create FormData object from the form
+
     
-
-    for (let [key, value] of formData.entries()) {
-        console.log(`${key}:`, value);
-    }
-
-    // Use fetch to send the data to the backend
     fetch('http://localhost:8000/api/v1/users/register', {
         method: 'POST',
-        body: formData
+        body: formData,  
     })
-    .then(response => response.json())  // Assuming the response is in JSON format
+    .then((response) => { response.json(),  console.log(response)})  
     .then(data => {
-        console.log('Success:', data);
-        // Handle success (e.g., show a success message or redirect)
+        console.log('Success:', data); 
     })
     .catch((error) => {
-        console.error('Error: !!', error);
-        // Handle error (e.g., show an error message)
+        console.error('Error: !!', error);  
     });
 });
 
